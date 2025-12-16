2b14579ca7bc4f23c9e4d47281b5ca7fd74c1506
beratsari <240101103@ogrenci.yalova.edu.tr>
2025-12-16 21:06:22 +0300

    Adjustment

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,7 @@
+namespace Api.Domain.Entities;
+
+public class User : BaseEntity{
+    public string Username {get; set;} = null!;
+    public string PasswordHash {get; set;} = null!;
+    public string Role {get; set;} = "User";
+    public ICollection<Review> Reviews {get; set;} = new List<Review>();}
