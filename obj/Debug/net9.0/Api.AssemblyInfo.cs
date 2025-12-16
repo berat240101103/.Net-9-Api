e04743b1538585c26545d25d59ae185671d87ed6
beratsari <240101103@ogrenci.yalova.edu.tr>
2025-12-16 19:47:35 +0300

    Layered architecture implemented and core entities with relationships defined

--- obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14c07ad370a88c6b34358985b59d65315ae1bb93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
