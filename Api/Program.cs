d86f277c2664831048f4fe02694b739ef7c5fdad
beratsari <240101103@ogrenci.yalova.edu.tr>
2025-12-19 18:37:44 +0300

    Api directory restored

---
+++ Api/Program.cs
@@ -0,0 +1,14 @@
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment()){app.UseSwagger();app.UseSwaggerUI();}
+app.MapGet("/", () => "API is running");
+
+app.Run();
+
